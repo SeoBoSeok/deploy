eb7d0f3abe83caaa9e98771d83aee92f6d32fade
ggy7bbo <ggybbo@codexbridge.com>
2017-05-30 11:19:55 +0900

    deploy with docker

---
+++ mup.js
@@ -0,0 +1,41 @@
+module.exports = {
+  servers: {
+    one: {
+      host: '52.79.105.59',
+      username: 'ubuntu',
+      pem: '/Users/Seoboseok/deploy.pem'
+      // password:
+      // or leave blank for authenticate from ssh-agent
+    }
+  },
+
+  meteor: {
+    name: 'owlclub',
+    path: '/Users/Seoboseok/GitHub/Owlclub/intermediate',
+    servers: {
+      one: {}
+    },
+    buildOptions: {
+      serverOnly: true,
+    },
+    env: {
+      ROOT_URL: 'http://djsocialclub.kr/',
+      // MONGO_URL: 'mongodb://ggybbo:gudwp4920@jello.modulusmongo.net:27017/Deda5gaq'
+      MONGO_URL: 'mongodb://localhost:27017/owlclub',
+      // MAIL_URL: 'smtp://ggy7bbo%40gmail.com:gudwp2084@smtp.gmail.com:587',
+      // MAIL_URL: 'smtp://postmaster%40sandboxdc64b29f64044dfbb7ce15c22ea4291d.mailgun.org:ca54aa55d6283a26da415c2167539ece@smtp.mailgun.org:587'
+      // "process.env.MAIL_URL"="smtp://ggy7bbo%40gmail.com:gudwp2084@smtp.gmail.com:465"
+    },
+
+    dockerImage: 'kadirahq/meteord:base',
+    deployCheckWaitTime: 60
+  },
+
+  mongo: {
+    oplog: true,
+    port: 27017,
+    servers: {
+      one: {},
+    },
+  },
+};
